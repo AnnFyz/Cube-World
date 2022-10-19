3d4aa530a86ea709ef049726413418e33bd7e386
AnnFyz <annfyz1232@yandex.ru>
2022-10-19 21:51:29 +0200

    add placing

--- Cube World/Assets/Scripts/BuildingsGrid.cs
+++ Cube World/Assets/Scripts/BuildingsGrid.cs
@@ -11,6 +11,17 @@ public class BuildingsGrid : MonoBehaviour
     private Building flyingBuilding;
     private Camera mainCamera;
 
+    private void OnDrawGizmos()
+    {
+        for (int x = 0; x < xGridSize; x++)
+        {
+            for (int y = 0; y < yGridSize; y++)
+            {
+                Gizmos.color = new Color(0, 1, 0, 03f);
+                Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1f, 0.1f, 1f));
+            }
+        }
+    }
     private void Awake()
     {
         gridSize = new Vector2Int(xGridSize, yGridSize);
@@ -22,7 +33,7 @@ public class BuildingsGrid : MonoBehaviour
     {
         if(flyingBuilding != null)
         {
-            Destroy(flyingBuilding);
+            Destroy(flyingBuilding.gameObject);
         }
 
         flyingBuilding = Instantiate(buildingPrefab);
@@ -32,16 +43,50 @@ public class BuildingsGrid : MonoBehaviour
     {
         if(flyingBuilding != null)
         {
-            var groundPlane = new Plane(Vector3.up, Vector3.zero); //endless ground
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            var groundPlane = new Plane(Vector3.up, Vector3.zero); //endless ground https://docs.unity3d.com/ScriptReference/Plane.html
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); // cursors position
 
-            if(groundPlane.Raycast(ray, out float position)) // if plane intersects with cursor
+            if (groundPlane.Raycast(ray, out float position)) // if plane intersects with cursor
             {
                 Vector3 worldPosition = ray.GetPoint(position); // position where Player clicked
+                int x = Mathf.RoundToInt(worldPosition.x);
+                int y = Mathf.RoundToInt(worldPosition.z);
+                Debug.Log(worldPosition);
+                bool avialable = true;
+                //if (x < 0 || x > gridSize.x - flyingBuilding.Size.x) avialable = false; //checking doesn't work correct
+                //if (y < 0 || y > gridSize.y - flyingBuilding.Size.y) avialable = false;
+                //if (avialable && IsPlaceTaken(x, y)) avialable = false;
+                flyingBuilding.transform.position = new Vector3(x, 0, y); // Building placing
 
-                flyingBuilding.transform.position = worldPosition;
+                if (avialable && Input.GetMouseButtonDown(0))
+                {
+                    PlaceFlyingBuilding(x,y);
+                }
             }
         }
+    }
+
+    private bool IsPlaceTaken(int placeX, int placeY)
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+            {
+                if(grid[placeX + x, placeY + y] != null) return true;
+            }
+        }
+        return false;
+    }
+    private void PlaceFlyingBuilding(int placeX, int placeY)
+    {
+        //for (int x = 0; x < flyingBuilding.Size.x; x++)
+        //{
+        //    for (int y = 0; y < flyingBuilding.Size.y; y++)
+        //    {
+        //        grid[placeX + x, placeY + y] = flyingBuilding;
+        //    }
+        //}
+        flyingBuilding = null;
     }
 }
 
